a2b29cdb55d4a6fb947f58c207da1ecfe0ea2103
nalro <nalrosoft@gmail.com>
2016-01-18 09:39:09 +0000

    aa

--- server.js
+++ server.js
@@ -1,36 +1,11 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-
-var database = require('./database.js');
-
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-var request = require("request");
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-//collection=userList&amp;criteria={"accountType":"dummy"}&amp;sort={}&amp;limit=500
-  
+/*
 io.on('connection', function (socket) {
+  console.log("connection");
+  
+  socket.on('disconnect', function () {
+       console.log("disconnect");
+    });
+    
     messages.forEach(function (data) {
       socket.emit('message', data);
     });
@@ -60,13 +35,13 @@ io.on('connection', function (socket) {
     });
 
     socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
+      socket.set('name', String(name || '손님'), function (err) {
         updateRoster();
       });
     });
   });
-
-function updateRoster() {
+ 
+ function updateRoster() {
   async.map(
     sockets,
     function (socket, callback) {
@@ -82,37 +57,49 @@ function broadcast(event, data) {
   sockets.forEach(function (socket) {
     socket.emit(event, data);
   });
-}
+}*/
 
-server.listen(7878, function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-  
-  
-  /*for (var i = 0; i < 100; i++) {
-    request("http://www.naver.com", function(error, response, body) {
-    if(error){
-      console.log("error = " + error);
-    } 
-    console.log(body);
-  });
-  }*/
-  
-  
-  
+//var async = require('async');
+//var express = require('express');
+ 
+//var api = require('./client/api');
+//var app = express();
+
+
+var http = require('http');  
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+  console.log("xxxx");
 });
-/*
 
-database.connect(function() {
-  // Start the application after the database connection is ready
-  //app.listen(3000);
-  console.log("database.connect");
-  setInterval(function(){
-    database.doSomethingWithDatabase(function(docs){
-      console.log((docs));
-    });
-    //console.log("xxxxx");
-  },5000); 
-}); */
+var addr = server.address();
+console.log("Chat server listening at", addr.address + ":" + addr.port);
+  
 
+
+
+var sockets[];
+var io = require('socket.io')(server);
+
+io.sockets.on('connection', function (socket) { 
+  console.log("connection"); 
+  
+  sockets.push(socket);
+  
+  socket.emit("hi", "hello");
+  
+  socket.on('disconnect', function () {
+    sockets.splice(sockets.indexOf(socket), 1);
+    printStatus();
+  }); 
+  
+}); 
+
+server.listen(7000);
+
+function printStatus() {
+  console.log("socket counht = " + sockets.length);
+}
  
